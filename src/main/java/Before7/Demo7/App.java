aa13edc7a3f73f65d78aefc804343821f011e858
Mehak Bandhral <mehakrajput8087@gmail.com>
2025-05-07 09:28:07 +0530

    Initial Commit

---
+++ src/main/java/Before7/Demo7/App.java
@@ -0,0 +1,13 @@
+package Before7.Demo7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
